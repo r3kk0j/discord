4f5898b4a581607436684da93a6649c8d05a26ce
Dominik <dominik.pindych@bisimulations.com>
2026-04-25 15:47:27 +0200

    .

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
+
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
-python manage.py makemigrations
+
+# Dodanie 'chat' jest kluczowe, żeby stworzyć tabelę chat_profile
+python manage.py makemigrations chat
 python manage.py migrate
