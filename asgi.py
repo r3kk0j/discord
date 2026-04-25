f8f0dbc05e16b2321444137dee488cdca126fbeb
Dominik <dominik.pindych@bisimulations.com>
2026-04-25 14:39:27 +0200

    .

--- asgi.py
+++ asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import routing  # Importuje plik routing.py
+import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
--- routing.py
+++ routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_async_view()),
+    # Poprawiono z as_async_view() na as_asgi()
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
