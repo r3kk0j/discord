68634c42e3ed3ad8862f50a325b08e090a78376d
Dominik <dominik.pindych@bisimulations.com>
2026-04-25 15:19:49 +0200

    .

--- asgi.py
+++ asgi.py
@@ -1,10 +1,17 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+# 1. Ustawienie modułu ustawień
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
+# 2. Inicjalizacja Django (Kluczowy krok!)
+django.setup()
+
+# 3. Importy po inicjalizacji
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
